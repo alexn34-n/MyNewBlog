6d5e6115987324ade351337e916dc46a1c68591a
alexn <alexn3469@mail.ru>
2023-01-15 15:00:15 +0300

    initial commit

---
+++ src/main/java/com/alexn/mynewblog/MyNewBlogApplication.java
@@ -0,0 +1,13 @@
+package com.alexn.mynewblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNewBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexn/mynewblog/MyNewBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexn.mynewblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNewBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
