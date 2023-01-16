18fdce5cec2ef40e292b4b668b833984b1001501
alexn <alexn3469@mail.ru>
2023-01-16 13:46:32 +0300

    encode password

--- src/main/java/com/alexn/mynewblog/service/AuthService.java
+++ src/main/java/com/alexn/mynewblog/service/AuthService.java
@@ -4,6 +4,9 @@ import com.alexn.mynewblog.dto.RegisterRequest;
 import com.alexn.mynewblog.model.User;
 import com.alexn.mynewblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +14,21 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
         user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword (registerRequest.getPassword()));
+
         userRepository.save(user);
     }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
 }
